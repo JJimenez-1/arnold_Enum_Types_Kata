b6d65a2e32b26e7d92668d317a59465686c1b827
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-04-01 16:02:47 +0200

    feat(ArnoldMain): Creada primera salida del programa

--- src/main/java/edu/elsmancs/arnoldEnumType/ArnoldMain.java
+++ src/main/java/edu/elsmancs/arnoldEnumType/ArnoldMain.java
@@ -1,5 +1,13 @@
 package edu.elsmancs.arnoldEnumType;
 
-public class ArnoldMain {
+import edu.elsmancs.arnoldEnumType.Planeta;
 
+public class ArnoldMain {
+    public static void main(String[] args) {
+        double peso = 1.0;
+
+        for(Planeta planeta : Planeta.values()) {
+            System.out.printf("Tu peso en %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+    }
 }
