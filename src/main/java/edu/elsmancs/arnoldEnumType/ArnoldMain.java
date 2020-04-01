35dc786c7bdb4aa3e6f731f823b71d52edb48605
JJimenez-1 <jjimenez@cifpfbmoll.eu>
2020-04-01 16:04:37 +0200

    feat(ArnoldMain): Creada segunda salida del programa

--- src/main/java/edu/elsmancs/arnoldEnumType/ArnoldMain.java
+++ src/main/java/edu/elsmancs/arnoldEnumType/ArnoldMain.java
@@ -7,7 +7,12 @@ public class ArnoldMain {
         double peso = 1.0;
 
         for(Planeta planeta : Planeta.values()) {
-            System.out.printf("Tu peso en %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+            System.out.printf("Tu peso en %s es %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
         }
+        System.out.println("\nTu peso solo en los planetas terrestres: ");
+        for(Planeta planeta : Planeta.getPlanetasTerrestres()){
+            System.out.printf("Tu peso en %s es %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+
     }
 }
